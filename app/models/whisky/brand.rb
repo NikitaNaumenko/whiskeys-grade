fa8516863577dd88dd43d4c9e16161833a3a5d5f
Nikita Naumenko <naumenkoniki@gmail.com>
2021-03-19 08:43:27 +0300

    add locales

--- app/models/whisky/brand.rb
+++ app/models/whisky/brand.rb
@@ -4,4 +4,9 @@ class Whisky::Brand < ApplicationRecord
   has_many :whiskies, class_name: 'Whisky', dependent: :destroy
   validates :name, uniqueness: { case_sensitive: true },
                    presence: true, length: { minimum: 5, maximum: 255 }
+  validates :country, presence: true
+
+  def to_s
+    name
+  end
 end
