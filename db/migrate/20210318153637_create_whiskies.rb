03e525eddb8f8a74d2dcafe3f815b0f2e87c48cf
Nikita Naumenko <naumenkoniki@gmail.com>
2021-03-22 19:54:25 +0300

    fix default

--- db/migrate/20210318153637_create_whiskies.rb
+++ db/migrate/20210318153637_create_whiskies.rb
@@ -5,7 +5,7 @@ class CreateWhiskies < ActiveRecord::Migration[6.1]
     create_table :whiskies do |t|
       t.string :title, index: { unique: true }
       t.text :description
-      t.integer :reviews_count
+      t.integer :reviews_count, default: 0
 
       t.timestamps
     end
